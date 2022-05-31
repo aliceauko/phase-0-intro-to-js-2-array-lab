3c690464d76b6c8fc7fb9b5c435b7bc26f1408e9
aliceauko <alice.auko@student.moringaschool.com>
2022-05-31 14:36:57 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph")
+    
+}
+
+function  destructivelyPrependCat(){
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+
+/*function appendCat(name){
+    var newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+  } */
+
+  function appendCat(name){
+    var newArray= [...cats,name];
+    return newArray;
+  }
+
+  function prependCat(name){
+    var newArray=[name, ...cats];
+    return newArray
+  }
+
+  function removeLastCat(){
+    var newArray= cats.slice(0,cats.length-1);
+    return newArray;
+  }
+  
+  function removeFirstCat(){
+    var newArray= cats.slice(1)
+    return newArray;
+  }
+  
+  
+
+
+
